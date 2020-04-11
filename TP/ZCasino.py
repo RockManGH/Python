61c17768e13e08b2122b937e31fc088d7a852bd2
RockManGH <62641877+RockManGH@users.noreply.github.com>
2020-04-11 09:49:36 +0200

    TP ZCasino avec fonction paire impaire

---
+++ TP/ZCasino.py
@@ -0,0 +1,96 @@
+# -*- coding: utf8 -*-
+# Jeu ZCasino
+
+import os
+import random
+import fonction_paire_impaire
+
+# boucle pour choix du numéro
+print ("\n Bienvenue au jeu ZCasino \n")
+
+jouer = True
+while jouer  :
+	
+	nbruser = -1
+	while nbruser <0 or nbruser > 49: 
+		
+		nbruser = input("\n Merci de choisir un numero entre 0 et 49 :")
+		
+		try :
+			#controle la saisie est bien un int 
+			int (nbruser)
+			# controle des chiffres négatif
+			assert int(nbruser) >= 0 
+			assert int (nbruser) <=49
+		except ValueError: 
+			print ("\nMerci de saisir un chiffre")	
+			nbruser = -1
+		except AssertionError :
+			print ("\nle chiffre choisi est invalide")
+			nbruser =-1
+		else : nbruser = int(nbruser)		
+
+
+	print ("\n vous avez choisi le numéro :", nbruser)
+
+	# le joueur doit saisir le somme à miser
+	bouvleremise = True
+	while bouvleremise  : 
+		
+		nbrmise = input("\nMerci de choisir la somme que vous voulez miser en $ :")
+		
+		try : 
+			int (nbrmise)
+			assert int (nbrmise) > 0
+		except ValueError: 
+			print ("\nMerci de saisir un chiffre")
+				
+		except AssertionError 	:
+			print ("\nMerci de saisir une somme supérieur à 0$: ")
+			
+		else : 
+			nbrmise = int(nbrmise)		
+			bouvleremise = False
+
+	print ("\nvous avez miser la somme de :", nbrmise)
+
+	
+	# choix random d'un chiffre entre 0 et 49
+	randnbr = random.randint(0,49)
+	print (" \nle chiffre gagnant est : ",randnbr)
+
+	# indiquer que le choix random est le gagnon
+	if randnbr == nbruser :
+		gain = nbrmise * 3
+		print (" \nvous avez gagner : ",gain,"$")	
+	else : 
+		print ("\n vous avez perdu")
+
+	npoim = fonction_paire_impaire.paire_impaire(nbruser)	
+	rpoim = fonction_paire_impaire.paire_impaire(randnbr)	
+	
+
+	# controler si le n° choisi et le random sont tous les deux paire ou impaire
+	if npoim == rpoim :
+
+		#si oui rendre 50% de la somme misé 
+		nbrmise = nbrmise /2
+		if npoim == True and rpoim == True :
+			print ("le chiffre entrer et le chiffre gagant sont tous les deux des paire, vous garder 50% de votre mise")
+		else :
+			print ("le chiffre entrer et le chiffre gagant sont tous les deux des impaire, vous garder 50% de votre mise")
+	#si non solde misée est perdu 
+	else :
+		print ("le chiffre entrer et le chiffre gagant sont des types différents, vous avez tout perdu ")
+		nbrmise =0
+
+	#afficher le solde du joueur 
+	print (" \nvotre solde actuelle est: ",nbrmise,"$")
+
+	# proposer de jouer à nouveau ou quitter 
+	jouer = input ("\n pour jouer à nouveau taper entrer si non Q pour quitter :")
+	if jouer.lower() == 'q': 		
+		jouer = False
+	else : jouer = True
+
+
---
+++ TP/fonction_paire_impaire.py
@@ -0,0 +1,10 @@
+
+def paire_impaire (nbr):
+
+	""" fonction pour controler si un chiffre et paire ou impaire """
+	if (nbr %2 == 0) :
+		nbr = True
+	else :
+		nbr = False
+
+	return (nbr)
